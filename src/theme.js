51940a5cdb0e5b8789976dcaa45c6272931c9b75
Karolina Marciniszyn <marciniszyn.karolina@gmail.com>
2022-01-25 18:13:42 +0100

    updating theme

--- src/theme.js
+++ src/theme.js
@@ -1,12 +1,15 @@
 export const theme = {
     colors: {
-        white: "#000000",
+        white: "#FFFFFF",
         whiteLilac: "#FBFBFE",
         blue: "#0366D6",
+        lightGray: "#E5E5E5",
         gray: "#6E7E91",
         black: "#252525",
         mineShaft: "#363636",
         iron: "#D1D5DA",
+        shadow: "rgba(9,10,51,0.03)",
+        secondShadow: "rgba(9,10,51,0.02)",
     },
     breakpoints: {
         maxMobileWidth: 767,
