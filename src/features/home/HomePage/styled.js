6b73d0595acdd90fa867f463631b2cf380807a3d
Karolina Marciniszyn <marciniszyn.karolina@gmail.com>
2022-01-25 18:37:44 +0100

    displaying HomePage Wrapper as a grid

--- src/features/home/HomePage/styled.js
+++ src/features/home/HomePage/styled.js
@@ -4,4 +4,11 @@ export const Wrapper = styled.div`
     max-width: 1220px;
     margin: 0 auto;
     padding: 14px;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-row-gap: 70px;
+
+    @media (max-width: ${({theme}) => theme.breakpoints.maxMobileWidth}px){
+        grid-row-gap: 50px;
+    }
 `;
