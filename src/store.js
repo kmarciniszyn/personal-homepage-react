c6ea67e2ec8e9ba085f77393d50fdb8db16768b9
Karolina Marciniszyn <marciniszyn.karolina@gmail.com>
2022-01-31 14:12:33 +0100

    adding store

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import themeReducer from './common/themeSlice';
+
+const store = configureStore({
+    reducer: {
+        theme: themeReducer,
+    },
+});
+
+
+export default store;
